ab6d6f73e439fa3aec60a7b49d07100192afaeb1
David Sanchez <fernandoge210895@gmail.com>
2021-11-19 12:41:31 -0500

    react-boilerplate

---
+++ src/components/Modal.jsx
@@ -0,0 +1,16 @@
+const Modal = () => {
+	return (
+		<div className='c-modal'>
+			<div className='c-modal__inner'>
+				<h4 className='c-modal__title'>Soy un modal</h4>
+				<div className='c-modal__wrapper-button'>
+					<button className='c-modal__button c-button c-button--big'>
+						Cerrar modal
+					</button>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Modal;
